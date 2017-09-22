cf0ebca56b57cb991c2da9276670132e9125aaa4
Runnan Zhong <r.tina.zhong@gmail.com>
2017-09-22 14:02:16 -0400

    devise taken out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       resources :collection_items, only: [:index, :update, :show, :create]
